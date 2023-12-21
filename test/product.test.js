e91fd2c5f1a24108cb5eb0173850992021f95e91
oussema.sellami <oussema.sellami@esprit.tn>
2023-12-21 13:45:40 +0100

    hhh

--- test/product.test.js
+++ test/product.test.js
@@ -1,43 +1,27 @@
-require('dotenv').config({ path: '.env.test' });
+require("dotenv").config({ path: ".env.test" });
 
-const chai = require('chai');
-const expect = chai.expect;
-const should = chai.should();
-const chaiHttp = require('chai-http');
-const server = require('../server');
-const Product = require('../product');
+const chai = require("chai");
 
-const mongoose = require('mongoose');
-
+const chaiHttp = require("chai-http");
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-
-
-
-describe('Test', () => {
-
-
-
-
-
-it('should POST a valid product', (done) => {
-        
+describe("Test", () => {
+  it("should POST a valid product", (done) => {
     let product = {
-        name: "Test Product",
-        price: 100,
-        quantity: 20
-    }
-    chai.request(server)
-    .post('/api/products')
-    .send(product)
-    .end((err, res) => {
+      name: "Test Product",
+      price: 100,
+      quantity: 20,
+    };
+    chai
+      .request(server)
+      .post("/api/products")
+      .send(product)
+      .end((err, res) => {
         res.should.have.status(201);
-        
+
         done();
-
-    });
-
-});
-
+      });
+  });
 });
